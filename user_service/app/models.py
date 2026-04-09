b6b4a68e94cfb921bdee8f24bef05fd8ae5de071
thushan-123 <thushanmadusanka456@gmail.com>
2026-04-09 21:27:19 +0530

    user table

--- user_service/app/models.py
+++ user_service/app/models.py
@@ -5,7 +5,7 @@ from db import Base
 class Admin(Base):
     __tablename__ = "admins"
 
-    id = Column(UUID, primary_key=True, index=True, unique=True)
+    admin_id = Column(UUID, primary_key=True, index=True, unique=True)
     username = Column(String, index=True, unique=True)
     first_name = Column(String)
     last_name = Column(String)
@@ -16,10 +16,21 @@ class Admin(Base):
 class Teacher(Base):
     __tablename__ = "teachers"
 
-    id = Column(UUID, primary_key=True, index=True, unique=True)
+    teacher_id = Column(UUID, primary_key=True, index=True, unique=True)
     teacher_username = Column(String , index=True, unique=True)
     first_name = Column(String)
     last_name = Column(String)
     password = Column(String)
-    created_at = Column(String)
-    updated_at = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+class Student(Base):
+    __tablename__ = "students"
+
+    student_id = Column(UUID, primary_key=True, index=True, unique=True)
+    student_username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
