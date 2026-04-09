2e44ce173a84723fefa29923bb5b424ac42407db
thushan-123 <thushanmadusanka456@gmail.com>
2026-04-09 21:15:44 +0530

    teacher table

--- user_service/app/models.py
+++ user_service/app/models.py
@@ -5,9 +5,21 @@ from db import Base
 class Admin(Base):
     __tablename__ = "admins"
 
-    id = Column(UUID, primary_key=True, index=True)
+    id = Column(UUID, primary_key=True, index=True, unique=True)
     username = Column(String, index=True, unique=True)
     first_name = Column(String)
     last_name = Column(String)
     password = Column(String)
     created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+class Teacher(Base):
+    __tablename__ = "teachers"
+
+    id = Column(UUID, primary_key=True, index=True, unique=True)
+    teacher_username = Column(String , index=True, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+    created_at = Column(String)
+    updated_at = Column(String)
