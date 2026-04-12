04d69f01feebd5e209c545d1bdf69ee8c0c633ae
thushan-123 <thushanmadusanka456@gmail.com>
2026-04-12 16:29:42 +0530

    imple passwore fun

--- backend/user_service/app/utility.py
+++ backend/user_service/app/utility.py
@@ -3,6 +3,7 @@ from jose import jwt , JWTError
 from fastapi.security import HTTPAuthorizationCredentials , HTTPBearer
 import os
 from dotenv import load_dotenv
+from passlib.context import CryptContext
 
 load_dotenv()
 
@@ -17,3 +18,12 @@ def current_user(token: HTTPAuthorizationCredentials = Depends(bearer_token)):
         return decode_data
     except JWTError as e:
         raise HTTPException(401, "invalid jwt")
+    
+
+passwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def password_hash(password: str) -> str:
+    return passwd_context.hash(password)
+
+def password_compare (current_password: str, hash_passwd: str) -> bool:
+    return passwd_context.verify(current_password, hash_passwd)
