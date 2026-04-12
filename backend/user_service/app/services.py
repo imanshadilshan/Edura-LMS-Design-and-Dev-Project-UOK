804e12b1b825f6ff6164f63d7e4179e367eb3d25
thushan-123 <thushanmadusanka456@gmail.com>
2026-04-12 16:41:01 +0530

    modify fun

--- backend/user_service/app/services.py
+++ backend/user_service/app/services.py
@@ -1,7 +1,13 @@
 from schema import TeacherCreate
 from db import get_db as db
+from utility import password_hash , password_compare
+from models import Teacher
 
 # teacher (create , update, )
 
 def create_teacher(data: TeacherCreate):
-    pass
+    data.password = password_hash(data.password)
+    teacher = Teacher(
+        teacher_username= data.teacher_username,
+        
+    )
