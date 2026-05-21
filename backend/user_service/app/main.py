90f0ac7835a72014c3797b3afff1d6b433442360
thushan-123 <thushanmadusanka456@gmail.com>
2026-05-21 07:12:24 +0530

    create admin get route

--- backend/user_service/app/main.py
+++ backend/user_service/app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def main():
     return {"message": "fastapi user_services"}
+
+@app.get("/admin/{admin_id}")
+async def admin(admin_id: int):
+    return {"message": "admin-data"}
