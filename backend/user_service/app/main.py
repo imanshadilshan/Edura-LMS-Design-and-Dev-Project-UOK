bcffedf213a11890999be013360eb0df7b53a769
thushan-123 <thushanmadusanka456@gmail.com>
2026-05-21 07:19:25 +0530

    create teacher

--- backend/user_service/app/main.py
+++ backend/user_service/app/main.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,10 +8,18 @@ app = FastAPI()
 async def main():
     return {"message": "fastapi user_services"}
 
+# admin endpoints
 @app.get("/admin/{admin_id}")
-async def admin(admin_id: int):
+async def admin(admin_id: UUID):
     return {"message": "admin-data"}
 
 @app.put("/admin/{admin_id}")
-async def admin(admin_id: int):
+async def admin(admin_id: UUID):
     return {"message": "admin-update"}
+
+
+# teacher endpoints
+
+@app.get("/teacher/{teacher_id}")
+async def user(teacher_id: UUID):
+    return {"message": "teacher-create"}
