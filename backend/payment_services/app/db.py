aca56e43f94eae4b98e7446f9dea9e597cbf8165
thushan-123 <thushanmadusanka456@gmail.com>
2026-06-08 14:42:02 +0530

    create db connection

---
+++ backend/payment_services/app/db.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+DATABASE_URL = "mysql+pymysql://thush:thushan2001@127.0.0.1/payment_service"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ backend/payment_services/app/models.py
@@ -0,0 +1,7 @@
+
+import uuid
+
+from sqlalchemy import Column, Integer, String, UUID, DateTime, Boolean, func
+
+
+class Payment(Base):
