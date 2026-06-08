a51a34a4f60bd698f0df7eb6827159a74653dea9
thushan-123 <thushanmadusanka456@gmail.com>
2026-06-08 14:53:07 +0530

    pament model

--- backend/payment_services/app/models.py
+++ backend/payment_services/app/models.py
@@ -12,3 +12,12 @@ class ClassPaymentPrice(Base):
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
+class Payment(Base):
+    __tablename__ = "payment"
+
+    payment_id = Column(UUID(as_uuid=True), primary_key=True, autoincrement=True)
+    student_id = Column(UUID(as_uuid=True), nullable=False)
+    class_id = Column(UUID(as_uuid=True), nullable=False)
+    date = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
