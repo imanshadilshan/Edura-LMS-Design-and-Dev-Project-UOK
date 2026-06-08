efbf0b064bd5a95eaa2e99583fd4be243922ae79
thushan-123 <thushanmadusanka456@gmail.com>
2026-06-08 14:50:58 +0530

    classpaymentprice table model

--- backend/payment_services/app/models.py
+++ backend/payment_services/app/models.py
@@ -1,7 +1,14 @@
 
 import uuid
 
-from sqlalchemy import Column, Integer, String, UUID, DateTime, Boolean, func
+from sqlalchemy import Column, Integer, String, UUID, DateTime, Boolean, func, Float
+from app.db import Base
 
+class ClassPaymentPrice(Base):
+    __tablename__ = "class_payment_price"
+    class_payment_price_id = Column(Integer, primary_key=True, autoincrement=True)
+    class_id = Column(UUID(as_uuid=True), nullable=False)
+    class_price = Column(Float, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
-class Payment(Base):
